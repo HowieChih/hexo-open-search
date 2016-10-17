d6b28d729c57a7df035864f27285440a84f0998b
howiechih <howiechih@qq.com>
2016-10-17 17:05:23 +0800

    done

--- src/main/java/me/qihao/App.java
+++ src/main/java/me/qihao/App.java
@@ -1,13 +1,22 @@
 package me.qihao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
- *
+ * 主函数
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    static {
+        LogInitializer.init();
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("start...");
+        HtmlProcessor.process("Chih's Blog");
+        logger.info("end.");
     }
 }
